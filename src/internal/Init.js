cf3f32c26ad918533271b46b0f6c3876be369b9f
torss <37229901+torss@users.noreply.github.com>
2019-01-25 06:40:59 +0100

    Add OrbitControls.js & Sky.js & GridHelper in Init.js

--- src/internal/Init.js
+++ src/internal/Init.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import GLTFLoader from 'three-gltf-loader'
+import {Sky} from './Sky'
+import {OrbitControls} from './OrbitControls'
 
 export function init (vueInstance) {
   const canvas = vueInstance.$refs.canvas
@@ -7,10 +9,26 @@ export function init (vueInstance) {
   const width = 1 // vueInstance.$el.clientWidth
   const height = 1 // vueInstance.$el.clientHeight
 
-  const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10)
-  camera.position.z = 1
+  const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 100)
+  camera.position.set(0, 1, 1)
 
   const scene = new THREE.Scene()
+
+  const helper = new THREE.GridHelper(10, 2, 0xffffff, 0xffffff)
+  scene.add(helper)
+
+  const controls = new OrbitControls(camera)
+
+  const sky = new Sky()
+  sky.scale.setScalar(10)
+  const skyUniforms = sky.material.uniforms
+  // skyUniforms.turbidity.value = 10
+  // skyUniforms.rayleigh.value = 2
+  // skyUniforms.luminance.value = 1
+  // skyUniforms.mieCoefficient.value = 0.005
+  // skyUniforms.mieDirectionalG.value = 0.8
+  skyUniforms.sunPosition.value.y = 10
+  scene.add(sky)
 
   const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
   scene.add(directionalLight)
@@ -52,6 +70,7 @@ export function init (vueInstance) {
 
     requestAnimationFrame(animate)
 
+    controls.update()
     if (mesh) {
       mesh.rotation.x += 0.01
       mesh.rotation.y += 0.02
