c0f0918b6ffb68f904ff765b389bebd92435e2ac
torss <37229901+torss@users.noreply.github.com>
2019-01-29 01:10:13 +0100

    Basic clean-up of TrackTest2.js

--- src/internal/TrackTest2.js
+++ src/internal/TrackTest2.js
@@ -38,20 +38,10 @@ class DriveWheelPlan {
     return getCircleTangentLinesVec(driveWheel0.center, driveWheel0.radius, driveWheel1.center, driveWheel1.radius, THREE.Vector3)
   }
 
-  constructor (center, radius, clockwise, trackAngle0, trackAngle1) {
+  constructor (center, radius, clockwise) {
     this.center = center
     this.radius = radius
     this.clockwise = clockwise
-    this.trackAngle0 = trackAngle0
-    this.trackAngle1 = trackAngle1 || (trackAngle0 + Math.PI) % (2 * Math.PI)
-    // this.trackPoint0 = undefined
-    this.trackPoint0 = this.getTrackPointFor(this.trackAngle0, 0)
-    this.trackPoint1 = this.getTrackPointFor(this.trackAngle1, 0)
-  }
-
-  getTrackPointFor (trackAngle, radiusOffset) {
-    return new THREE.Vector2(this.center.x + this.radius + radiusOffset, this.center.y)
-      .rotateAround(new THREE.Vector2(this.center.x, this.center.y), trackAngle)
   }
 
   generate (depth, material) {
@@ -96,9 +86,6 @@ class TrackDrivePlan {
       curveSegments: 64
     }
     const geometry = new THREE.ExtrudeBufferGeometry(shape, extrudeSettings)
-    // const mesh = new THREE.Mesh(geometry, material)
-    // // mesh.position.set(-0.5 * width, 0, -0.5 * depth)
-    // parent.add(mesh)
     parent.add(new THREE.LineSegments(new THREE.WireframeGeometry(geometry), material))
   }
 
@@ -108,27 +95,12 @@ class TrackDrivePlan {
       console.warn('TrackDrivePlan.driveWheels.length < 2')
       return curvePath
     }
-    // for (const driveWheel of this.driveWheels) {
-    //   driveWheel.calcTrackPoint(this.thickness * 0.5)
-    // }
-    // window.curvePath = curvePath // DEBUG
     for (let i = 1; i < this.driveWheels.length; ++i) {
       const prev = this.driveWheels[i - 1]
       const cur = this.driveWheels[i]
 
-      // const dir0 = new THREE.Vector2().subVectors(cur.center, prev.center)
-      // const dir1 = new THREE.Vector2().subVectors(prev.center, cur.center)
-      // console.log('dir0', dir0)
       for (const tangentLine of DriveWheelPlan.getTangentLines(prev, cur)) {
-        // const dirTlp0 = new THREE.Vector2().copy(tangentLine[0]).sub(prev.center)
-        // const dot0 = dirTlp0.sub(prev.center).dot(dir0)
-        // console.log('dirTlp0', dirTlp0, dot0)
-        // if ((dot0 < 0) !== prev.clockwise) continue
-        // console.log('dirTlp0', dirTlp0)
         if ((sideOfLine(tangentLine[0], prev.center, cur.center) < 0) !== prev.clockwise) continue
-        // const dirTlp1 = new THREE.Vector2().copy(tangentLine[1]).sub(cur.center)
-        // const dot1 = dirTlp1.sub(cur.center).dot(dir1)
-        // if ((dot1 < 0) === cur.clockwise) continue
         if ((sideOfLine(tangentLine[1], prev.center, cur.center) < 0) !== cur.clockwise) continue
         if (i >= 2) {
           const prevCurve = curvePath.curves[i - 2]
@@ -139,32 +111,14 @@ class TrackDrivePlan {
 
           const arcOffset = new THREE.Vector2().copy(p1).sub(center).angle()
           let arcLength = new THREE.Vector2().copy(p2).sub(center).angle()
-          window.d1 = new THREE.Vector2().copy(p1).sub(center)
-          window.d2 = new THREE.Vector2().copy(p2).sub(center)
-          // console.log('DEBUG', arcOffset, arcLength)
           arcLength -= arcOffset
           if ((arcLength < 0) !== clockwise) {
             arcLength += 2 * Math.PI
           }
           curvePath.add(new ArcCurve(cur.radius, arcLength, arcOffset, center))
-
-          // const control0 = new THREE.Vector3().lerpVectors(p1, p2, 0.5)
-          // curvePath.add(new THREE.CubicBezierCurve3(p2, control0, control0, p1))
         }
         curvePath.add(new THREE.LineCurve3(tangentLine[0], tangentLine[1]))
       }
-
-      // const p0 = new THREE.Vector3()
-      // const p1 = new THREE.Vector3()
-      // copyXy(prev.trackPoint0, p0)
-      // copyXy(cur.trackPoint0, p1)
-      // curvePath.add(new THREE.LineCurve3(p0, p1))
-      // const p2 = new THREE.Vector3()
-      // copyXy(cur.trackPoint1, p2)
-      // const dir = new THREE.Vector3().subVectors(p1, p0).normalize().multiplyScalar(cur.radius)
-      // const p1r = p1.clone().add(dir)
-      // const p2r = p2.clone().add(dir)
-      // curvePath.add(new THREE.CubicBezierCurve3(p1, p1r, p2r, p2))
     }
     return curvePath
   }
@@ -222,7 +176,6 @@ function genTrackCrossSection (path, x, y, width, height, radius) {
   path.moveTo(x, y)
   path.lineTo(x + width, y)
   path.bezierCurveTo(x + width + radius, y, x + width + radius, y + height, x + width, y + height)
-  // path.lineTo(x + width, y + height)
   path.lineTo(x, y + height)
   path.bezierCurveTo(x - radius, y + height, x - radius, y, x, y)
   return path
@@ -266,48 +219,12 @@ export function trackTest (scene, materialParam) {
   const thickness = 0.05
   const roundedWidth = 0.05
 
-  const shape = new THREE.Shape()
-  genTrackCrossSection(shape, 0, 0, depth, thickness, roundedWidth)
-
-  const extrudeSettings = {
-    extrudePath: genTrackExtrudePath(0, 0, width, height, radius),
-    steps: 200,
-    // depth,
-    bevelEnabled: false,
-    curveSegments: 64
-  }
-
-  const geometry = new THREE.ExtrudeBufferGeometry(shape, extrudeSettings)
-  // adjustGeometryNormals(geometry)
-  const mesh = new THREE.Mesh(geometry, material)
-  // mesh.position.set(-0.5 * width, 0, -0.5 * depth)
-  // scene.add(mesh)
-
-  const protrusion = 0.025
-  const extrudeSettings2 = {
-    extrudePath: genDriveWheelExtrudePath(height - thickness), // extrudeSettings.extrudePath, // genDriveWheelExtrudePath(height),
-    steps: 64,
-    // depth: depth + roundedWidth,
-    bevelEnabled: false,
-    curveSegments: 64
-  }
-  //   delete extrudeSettings.extrudePath //
-  const shape2 = genDriveWheelCrossSection(depth + roundedWidth + protrusion, height - thickness, protrusion) // shape
-  const geometry2 = new THREE.ExtrudeBufferGeometry(shape2, extrudeSettings2)
-  const xos = 3
-  const xowidth = (width + 1.5 * (height - thickness)) / xos
-  for (let x = 0; x < xos; ++x) {
-    const mesh2 = new THREE.Mesh(geometry2, material2)
-    mesh.add(mesh2)
-    mesh2.position.set(radius - 0.5 * (height - thickness) + x * xowidth, thickness, 0)
-  }
-
   // - //
 
   const trackDrivePlan = new TrackDrivePlan(depth, thickness, roundedWidth)
   const driveWheelRadius = 0.1
-  trackDrivePlan.driveWheels.push(new DriveWheelPlan(new THREE.Vector3(0, 0, 0), driveWheelRadius, true, Math.PI * 0.5))
-  trackDrivePlan.driveWheels.push(new DriveWheelPlan(new THREE.Vector3(width * 0.5, width * 0.25, 0), driveWheelRadius, true, Math.PI * 0.5))
-  trackDrivePlan.driveWheels.push(new DriveWheelPlan(new THREE.Vector3(width, 0, 0), driveWheelRadius, true, Math.PI * 0.5))
+  trackDrivePlan.driveWheels.push(new DriveWheelPlan(new THREE.Vector3(0, 0, 0), driveWheelRadius, true))
+  trackDrivePlan.driveWheels.push(new DriveWheelPlan(new THREE.Vector3(width * 0.5, width * 0.25, 0), driveWheelRadius, true))
+  trackDrivePlan.driveWheels.push(new DriveWheelPlan(new THREE.Vector3(width, 0, 0), driveWheelRadius, true))
   trackDrivePlan.generate(scene, material, material2)
 }
