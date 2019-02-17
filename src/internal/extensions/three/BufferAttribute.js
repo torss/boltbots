c7a207a3bdea1ab86b259fb846737b3b68818f32
torss <37229901+torss@users.noreply.github.com>
2019-02-17 20:02:20 +0100

    Add BufferAttributeExtIndex

--- src/internal/extensions/three/BufferAttribute.js
+++ src/internal/extensions/three/BufferAttribute.js
@@ -16,6 +16,13 @@ THREE.BufferAttribute.prototype.resize = function (newCount) {
   return this
 }
 
+function pushBase (bufferAttributeExt, itemSizeReq, funcName, data, assignFunc) {
+  if (bufferAttributeExt.itemSize !== itemSizeReq) throw new TypeError('BufferAttributeExt: Can\'t use ' + funcName + ' since itemSize !== ' + itemSizeReq)
+  bufferAttributeExt.padSize(bufferAttributeExt.countCurrent + data.length)
+  for (let i = 0; i < data.length; ++i) assignFunc(data[i])
+  return bufferAttributeExt
+}
+
 /**
  * Extended THREE.BufferAttribute for progressive resizing
  */
@@ -39,16 +46,53 @@ export class BufferAttributeExt extends THREE.BufferAttribute {
   }
 
   pushVector3 (...vector3s) {
-    if (this.itemSize !== 3) throw new TypeError('BufferAttributeExt: Can\'t use pushVector3 since itemSize !== 3')
-    this.padSize(this.countCurrent + vector3s.length)
-    let index = this.indexCurrent
-    for (let i = 0; i < vector3s.length; ++i) {
-      const vector3 = vector3s[i]
-      this.array[index++] = vector3.x
-      this.array[index++] = vector3.y
-      this.array[index++] = vector3.z
+    return pushBase(this, 3, 'pushVector3', vector3s, (vector3) => {
+      this.array[this.indexCurrent++] = vector3.x
+      this.array[this.indexCurrent++] = vector3.y
+      this.array[this.indexCurrent++] = vector3.z
+    })
+  }
+}
+
+/**
+ * BufferAttributeExt for indices
+ */
+export class BufferAttributeExtIndex extends BufferAttributeExt {
+  constructor (array = new Uint16Array()) {
+    super(array, 1)
+    this.highestStoredIndex = -1
+  }
+
+  fitSize () {
+    const is32bit = this.array instanceof Uint32Array
+    const needs32bit = this.highestStoredIndex > 2 ** 16 - 1
+    if (is32bit && !needs32bit) {
+      this.array = new Uint16Array(this.array.subarray(0, this.countCurrent))
+      this.count = this.countCurrent
+      this.indexCurrent = this.countCurrent * this.itemSize
+    } else {
+      super.fitSize()
     }
-    this.indexCurrent = index
     return this
+  }
+
+  pushWithOffset (offset, ...indices) {
+    let is32bit = this.array instanceof Uint32Array
+    return pushBase(this, 1, 'pushVector3', indices, (index) => {
+      index += offset
+      if (!is32bit) {
+        const needs32bit = index > 2 ** 16 - 1
+        if (needs32bit) {
+          this.array = new Uint32Array(this.array)
+          is32bit = true
+        }
+      }
+      this.array[this.indexCurrent++] = index
+      if (this.highestStoredIndex < index) this.highestStoredIndex = index
+    })
+  }
+
+  pushRelative (...indices) {
+    return this.pushWithOffset(this.highestStoredIndex + 1, ...indices)
   }
 }
