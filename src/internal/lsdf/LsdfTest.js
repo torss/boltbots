720d05dd7d71ab3f016f792fe4e93ad98de9f61d
torss <37229901+torss@users.noreply.github.com>
2019-03-01 01:10:05 +0100

    Factor out addTestPlane in LsdfTest.js

--- src/internal/lsdf/LsdfTest.js
+++ src/internal/lsdf/LsdfTest.js
@@ -4,6 +4,22 @@ import vertexShader from './sample.vert.glsl'
 import fragmentShader from './sample.frag.glsl'
 
 export function lsdfTest (vueInstance, scene, camera, materialParam) {
+  const material = new THREE.RawShaderMaterial({
+    uniforms: {
+      time: { value: 1.0 }
+    },
+    vertexShader,
+    fragmentShader,
+    side: THREE.FrontSide,
+    transparent: true
+  })
+  addTestPlane(scene, material)
+
+  const intervalId = setInterval(() => { material.uniforms.time.value = performance.now() * 0.005 }, 16)
+  vueInstance.$deinit.push(() => clearInterval(intervalId))
+}
+
+function addTestPlane (scene, material) {
   const geometry = new THREE.PlaneBufferGeometry()
   const colorAttribute = new THREE.Uint8BufferAttribute([...Array(4 * 4)].map(() => Math.floor(0xff * Math.random())), 4)
   colorAttribute.normalized = true
@@ -13,18 +29,7 @@ export function lsdfTest (vueInstance, scene, camera, materialParam) {
   uvRange.forEach(u => uvRange.forEach(v => uvs.push(u, v)))
   const uvAttribute = new THREE.Float32BufferAttribute(uvs, 2)
   geometry.addAttribute('uv', uvAttribute)
-  const material = new THREE.RawShaderMaterial({
-    uniforms: {
-      time: { value: 1.0 }
-    },
-    vertexShader,
-    fragmentShader,
-    side: THREE.DoubleSide,
-    transparent: true
-  })
+
   const plane = new THREE.Mesh(geometry, material)
   scene.add(plane)
-
-  const intervalId = setInterval(() => { material.uniforms.time.value = performance.now() * 0.005 }, 16)
-  vueInstance.$deinit.push(() => clearInterval(intervalId))
 }
