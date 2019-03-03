881bb93d239a13664edeabb9df9a5945e7a522e8
torss <37229901+torss@users.noreply.github.com>
2019-03-03 06:32:58 +0100

    Minor LSDF approach5 JS cleanup

--- src/internal/lsdf/approach5/LsdfTest.js
+++ src/internal/lsdf/approach5/LsdfTest.js
@@ -2,8 +2,7 @@
 import * as THREE from 'three'
 import vertexShader from './sample.vert.glsl'
 import fragmentShader from './sample.frag.glsl'
-import {moctCubeSides, moctOctants} from '../../moctree'
-import {BufferAttributeExtIndex, BufferAttributeExt} from '../../extensions'
+import {BufferAttributeExt} from '../../extensions'
 import '../../extensions/three/Vector3'
 import {lsdfOpTypes, initTestLsdfConfigs} from '../LsdfOpType'
 import {LoctTree} from './LoctTree'
@@ -19,7 +18,7 @@ export function lsdfTest (vueInstance, scene, camera, materialParam, renderer) {
     side: THREE.FrontSide,
     transparent: false
   })
-  addTestCube(scene, material)
+  addTestShapes(scene, material)
 
   const intervalId = setInterval(() => {
     // WebGLRenderer.js - isPointsMaterial - refreshUniformsPoints
@@ -30,17 +29,14 @@ export function lsdfTest (vueInstance, scene, camera, materialParam, renderer) {
   vueInstance.$deinit.push(() => clearInterval(intervalId))
 }
 
-function addTestCube (scene, material) {
-  const geometry = createCubeGeometry(material)
-  // const cube = new THREE.Mesh(geometry, material)
-  // scene.add(cube)
-
+function addTestShapes (scene, material) {
+  const geometry = createGeometry(material)
   // material = new THREE.PointsMaterial({ size: 0.0125, sizeAttenuation: true, vertexColors: THREE.VertexColors })
   const points = new THREE.Points(geometry, material)
   scene.add(points)
 }
 
-function createCubeGeometry (material) {
+function createGeometry (material) {
   const geometry = new THREE.BufferGeometry()
   const buffers = {
     position: new BufferAttributeExt(new Float32Array(), 3),
@@ -145,112 +141,12 @@ function refineLoctNodeSplit (maxDepth, sdfEpsilon, sdfFunc, postSplitFunc, loct
   if (positive && negative) {
     postSplitFunc(loctNode, loctNodeOrigin)
   }
-  // else {
-  //   loctNode.subs.length = 0 // TODO proper recursive merge
-  // }
   loctNode.subs.length = 0
 }
 
 function refineLoctNode (maxDepth, sdfEpsilon, sdfFunc, postSplitFunc, loctNode, loctNodeOrigin) {
-  // refineLoctNodeObtain(sdfFunc, loctNode, loctNodeOrigin) // loctNode.sdfValue = sdfFunc(loctNodeOrigin)
-  // const sdfValueAbs = Math.abs(loctNode.sdfValue)
-  // if (sdfValueAbs < loctNode.level.diagonalHalf && sdfValueAbs > sdfEpsilon && loctNode.level.depth < maxDepth) {
-  //   refineLoctNodeSplit(maxDepth, sdfEpsilon, sdfFunc, postSplitFunc, loctNode, loctNodeOrigin)
-  // }
-  // // else { leafFunc(loctNode, loctNodeOrigin) }
-
   const sdfValueAbs = Math.abs(loctNode.sdfValue)
   if (sdfValueAbs < 2 * loctNode.level.diagonalHalf && loctNode.level.depth < maxDepth) {
     refineLoctNodeSplit(maxDepth, sdfEpsilon, sdfFunc, postSplitFunc, loctNode, loctNodeOrigin)
   }
-}
-
-function addCubeFaces (origin, shapeType, indices, positions, relposs, normals, uvs, shapeTypes, diagonalHalfs, diagonalHalf, scales, scale = 0.5) {
-  moctCubeSides.forEach((moctCubeSide) => {
-    const points = moctCubeSide.faceDrawCc.map(facePoint => origin.clone().addScaledVector(facePoint, scale))
-    indices.pushRelative(0, 1, 2, 1, 3, 2)
-    positions.pushVector3(points[0], points[1], points[2], points[3])
-    relposs.pushVector3(moctCubeSide.faceDrawCc[0], moctCubeSide.faceDrawCc[1], moctCubeSide.faceDrawCc[2], moctCubeSide.faceDrawCc[3])
-    const normal = moctCubeSide.normal
-    normals.pushVector3(normal, normal, normal, normal)
-    const uvRange = [0, 1]
-    uvRange.forEach(u => uvRange.forEach(v => uvs.pushVector2(new THREE.Vector2(u, v))))
-    shapeTypes.pushVector3(shapeType, shapeType, shapeType, shapeType)
-    diagonalHalfs.pushScalar(diagonalHalf, diagonalHalf, diagonalHalf, diagonalHalf)
-    scales.pushScalar(scale, scale, scale, scale)
-    // lsdfConfigs.pushScalar(lsdfConfig, lsdfConfig, lsdfConfig, lsdfConfig)
-  })
-}
-
-function initTestTextureFromLsdfConfigs (lsdfConfigs) {
-  const textureSize = new THREE.Vector2(lsdfConfigs.length, lsdfConfigs.length)
-  const size = textureSize.x * textureSize.y
-  const data = new Float32Array(4 * size)
-  for (let i = 0; i < data.length; ++i) data[i] = 0
-  let i
-  let stackIndex
-  const writeData = (lsdfConfig) => {
-    const opType = lsdfOpTypes[lsdfConfig.type]
-    if (opType.kind === 'combine') {
-      const x = writeData(lsdfConfig.x)
-      const y = writeData(lsdfConfig.y)
-      data[i++] = x
-      data[i++] = y
-      data[i++] = 0
-      data[i++] = opType.opCode
-      switch (lsdfConfig.type) {
-        case 'unionSmooth':
-        case 'subtractSmooth':
-        case 'intersectSmooth':
-          data[i++] = lsdfConfig.radius
-          data[i++] = 0
-          data[i++] = 0
-          data[i++] = 0
-          break
-        default:
-          data[i++] = 0
-          data[i++] = 0
-          data[i++] = 0
-          data[i++] = 0
-      }
-    } else {
-      if (opType.typeMapSize <= 0) console.warn('W190302-0239') // Shouldn't actually be possible atm
-      data[i++] = lsdfConfig.position.x
-      data[i++] = lsdfConfig.position.y
-      data[i++] = lsdfConfig.position.z
-      data[i++] = opType.opCode
-      switch (lsdfConfig.type) {
-        case 'sphere':
-          data[i++] = lsdfConfig.radius
-          data[i++] = 0
-          data[i++] = 0
-          data[i++] = 0
-          break
-        case 'box':
-          data[i++] = lsdfConfig.size.x
-          data[i++] = lsdfConfig.size.y
-          data[i++] = lsdfConfig.size.z
-          data[i++] = 0
-          break
-        default:
-          console.warn('Unknown ' + opType.kind + ' lsdfConfig.type: ' + lsdfConfig.type)
-          data[i++] = 0
-          data[i++] = 0
-          data[i++] = 0
-          data[i++] = 0
-      }
-    }
-    if (stackIndex + 1 > 16) console.error('E190302-0332 LSDF complexity too high')
-    return stackIndex++
-  }
-  for (let lsdfConfigIndex = 0; lsdfConfigIndex < lsdfConfigs.length; ++lsdfConfigIndex) {
-    i = lsdfConfigIndex * textureSize.x * 4
-    stackIndex = 0
-    const lsdfConfig = lsdfConfigs[lsdfConfigIndex]
-    writeData(lsdfConfig)
-    for (let j = 0; j < 8; ++j) data[i++] = 0
-  }
-  const texture = new THREE.DataTexture(data, textureSize.x, textureSize.y, THREE.RGBAFormat, THREE.FloatType)
-  texture.needsUpdate = true
-  return {texture, textureSize}
 }
