966acd70fe331192e4272c58e16bbfa520a90518
torss <37229901+torss@users.noreply.github.com>
2019-05-17 01:17:17 +0200

    Increase conscepterTest testConstruct iterations

--- src/internal/conscepter/ConscepterTest.js
+++ src/internal/conscepter/ConscepterTest.js
@@ -8,7 +8,7 @@ import {testConstruct} from './approach1'
 export function conscepterTest (vueInstance, scene, camera, material, renderer, preAnimateFuncs) {
   const bufferSet = new BufferSet()
   testConstruct(bufferSet)
-  for (let i = 0; i < 2; ++i) testConstruct(new BufferSet()) // Timer test
+  for (let i = 0; i < 10; ++i) testConstruct(new BufferSet()) // Timer test
   const mesh = new THREE.Mesh(bufferSet.fitSize().createGeometry(), material)
   scene.add(mesh)
   const material2 = new THREE.MeshStandardMaterial({
