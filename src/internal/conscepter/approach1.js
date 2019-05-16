e2b5496baf7ff69d12c52181e5e9310d0d6c7f9c
torss <37229901+torss@users.noreply.github.com>
2019-05-17 01:18:29 +0200

    Add incomplete CsFraw

--- src/internal/conscepter/approach1.js
+++ src/internal/conscepter/approach1.js
@@ -2,9 +2,14 @@
 import * as THREE from 'three'
 import {CsBuf} from './CsBuf'
 import {CsPipe, CsPipeCmd} from './CsPipe'
+import {CsFraw} from './CsFraw'
 
 export function testConstruct (bufferSet) {
-  console.time('testConstruct-1')
+  testConstruct2(bufferSet)
+}
+
+export function testConstruct1 (bufferSet) {
+  console.time('testConstruct-1-1')
   const csBuf = new CsBuf({default: bufferSet})
   const rotation = new THREE.Matrix3().makeRotationFromQuaternion(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), 0.5 * Math.PI))
   new CsPipe(128).runCmds(csBuf, [
@@ -23,5 +28,12 @@ export function testConstruct (bufferSet) {
     new CsPipeCmd().makeLength(-0.375),
     new CsPipeCmd().makeRadius(0.05)
   ])
-  console.timeEnd('testConstruct-1')
+  console.timeEnd('testConstruct-1-1')
+}
+
+export function testConstruct2 (bufferSet) {
+  console.time('testConstruct-1-2')
+  const csBuf = new CsBuf({default: bufferSet})
+  new CsFraw().testConstruct(csBuf) // TODO
+  console.timeEnd('testConstruct-1-2')
 }
