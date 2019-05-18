970850f8834c52e6a88c8c5716df988e43fe299c
torss <37229901+torss@users.noreply.github.com>
2019-05-18 17:12:13 +0200

    Add shared BufferSet btileTest variant

--- src/internal/btile/BtileTest.js
+++ src/internal/btile/BtileTest.js
@@ -13,68 +13,72 @@ export function btileTest (vueInstance, scene, camera, material, renderer, preAn
   const gltfLoader = new GLTFLoader()
   gltfLoader.load('../statics/models/TestCube.glb', (gltf) => {
     // window.scene = gltf.scene
+    const copyDim = 3
 
-    let geometry
-    gltf.scene.traverseVisible(obj => {
-      if (obj.isMesh) obj.material = obj.name.endsWith('label') ? material : material2
-      if (obj.isMesh) geometry = obj.geometry
-    })
-
-    const copyDim = 128
-
+    // // Shared BufferSet(s) variant:
+    const bufferSets = {}
+    const getBufferSet = materialKey => (bufferSets[materialKey] = bufferSets[materialKey] || new BufferSet())
     console.time('btile-bufferSet-copyDim-' + copyDim)
-    const bufferSet = new BufferSet()
-    const copyAppendMod = (attrKey, mod = undefined) => {
-      const frBuf = geometry.getAttribute(attrKey)
-      const toBuf = bufferSet[attrKey]
-      toBuf.padSize(toBuf.countCurrent + frBuf.count)
-      if (mod) {
-        if (frBuf.itemSize === 3) {
-          for (let i = 0; i < frBuf.array.length; i += 3) {
-            toBuf.upushVector3(mod(new THREE.Vector3(frBuf.array[i + 0], frBuf.array[i + 1], frBuf.array[i + 2])))
+    // gltf.scene.updateMatrixWorld()
+    gltf.scene.traverseVisible(obj => {
+      if (obj.isMesh) {
+        const bufferSet = getBufferSet(obj.name.endsWith('label') ? 'material' : 'material2')
+        const geometry = obj.geometry
+        // geometry.applyMatrix(obj.matrixWorld)
+
+        const copyAppendMod = (attrKey, mod = undefined) => {
+          const frBuf = geometry.getAttribute(attrKey)
+          const toBuf = bufferSet[attrKey]
+          toBuf.padSize(toBuf.countCurrent + frBuf.count)
+          if (mod) {
+            if (frBuf.itemSize === 3) {
+              for (let i = 0; i < frBuf.array.length; i += 3) {
+                toBuf.upushVector3(mod(new THREE.Vector3(frBuf.array[i + 0], frBuf.array[i + 1], frBuf.array[i + 2])))
+              }
+            } else {
+              console.error('copyAppendMod not implemented for frBuf.itemSize === ' + frBuf.itemSize)
+            }
+          } else {
+            for (let i = 0; i < frBuf.array.length; ++i) {
+              toBuf.array[toBuf.indexCurrent++] = frBuf.array[i]
+            }
           }
-        } else {
-          console.error('copyAppendMod not implemented for frBuf.itemSize === ' + frBuf.itemSize)
         }
-      } else {
-        for (let i = 0; i < frBuf.array.length; ++i) {
-          toBuf.array[toBuf.indexCurrent++] = frBuf.array[i]
+        for (let x = 0; x < copyDim; ++x) {
+          for (let y = 0; y < copyDim; ++y) {
+            const posAdd = new THREE.Vector3(x * 2, 0, y * 2)
+            copyAppendMod('position', pos => pos.add(posAdd)) // FIXME add relative mesh offsets!
+            copyAppendMod('normal')
+
+            const vertexCount = geometry.getAttribute('position').count
+            bufferSet.color.padSize(bufferSet.color.countCurrent + vertexCount)
+            for (let i = 0; i < vertexCount; ++i) bufferSet.color.upushVector3(new THREE.Vector3(1, 0, 0))
+
+            bufferSet.index.pushRelative(...geometry.index.array)
+          }
         }
       }
-    }
-    for (let x = 0; x < copyDim; ++x) {
-      for (let y = 0; y < copyDim; ++y) {
-        const posAdd = new THREE.Vector3(x * 2, 0, y * 2)
-        copyAppendMod('position', pos => pos.add(posAdd))
-        copyAppendMod('normal')
-
-        const vertexCount = geometry.getAttribute('position').count
-        bufferSet.color.padSize(bufferSet.color.countCurrent + vertexCount)
-        for (let i = 0; i < vertexCount; ++i) bufferSet.color.upushVector3(new THREE.Vector3(1, 0, 0))
-
-        bufferSet.index.pushRelative(...geometry.index.array)
-        // const indexTo = bufferSet.index
-        // const indexFr = geometry.index
-        // const hsi = indexTo.highestStoredIndex + 1
-        // for (let i = 0; i < indexFr.array.length; ++i) {
-        //   indexTo.pushWithOffset(hsi, indexFr.array[i])
-        // }
-      }
-    }
+    })
     console.timeEnd('btile-bufferSet-copyDim-' + copyDim)
 
-    scene.add(new THREE.Mesh(bufferSet.fitSize().createGeometry(), material))
+    for (const [materialKey, bufferSet] of Object.entries(bufferSets)) {
+      const materialSel = materialKey === 'material' ? material : material2
+      scene.add(new THREE.Mesh(bufferSet.fitSize().createGeometry(), materialSel))
+    }
     // window.bufferSet = bufferSet
     // window.geometry = geometry
 
-    // Naive aka draw-call-heavy variant:
-    // for (let x = 0; x < copyDim; ++x) {
-    //   for (let y = 0; y < copyDim; ++y) {
-    //     const clone = gltf.scene.clone()
-    //     clone.position.x += x * 2
-    //     clone.position.z += y * 2
-    //     scene.add(clone)
-    //   }
-    // }
+    // // Naive aka draw-call-heavy variant:
+    gltf.scene.traverseVisible(obj => {
+      if (obj.isMesh) obj.material = obj.name.endsWith('label') ? material : material2
+    })
+    for (let x = 0; x < copyDim; ++x) {
+      for (let y = 0; y < copyDim; ++y) {
+        const clone = gltf.scene.clone()
+        clone.position.x -= (x + 2) * 2
+        clone.position.z -= (y + 2) * 2
+        scene.add(clone)
+      }
+    }
   }, undefined, console.error)
 }
